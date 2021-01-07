ef537ce2f69887660f861b6869f7b6847e69ccb6
choisaywhy <sehwa98@icloud.com>
2021-01-07 22:28:16 +0900

    Hide secret key

--- django_react/settings.py
+++ django_react/settings.py
@@ -21,7 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_dk7@b+vbcdzl@10jj3x-h5j0v9wl=fy20h^zttitcjeb6#hsu'
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "에러 {}".format(setting)
+        raise ImproperlyConfigured(error_msg)
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
